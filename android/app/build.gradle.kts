968fc82cd9bb3639f31d7f9674ee2f0e861eda6d
Ahn ByeongHyeon <themechanic002@gmail.com>
2025-09-19 16:34:42 +0900

    Update: import

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
